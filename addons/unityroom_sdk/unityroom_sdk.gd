942ee15b3edf7a9d502424e73d74ffafbbfc2bbe
nuskey8 <nuskey.md@gmail.com>
2026-06-09 16:23:52 +0900

    fix: remove autoload

--- addons/unityroom_sdk/unityroom_sdk.gd
+++ addons/unityroom_sdk/unityroom_sdk.gd
@@ -1,21 +1,14 @@
 @tool
 extends EditorPlugin
 
-const AUTOLOAD_NAME := "UnityroomClient"
-const AUTOLOAD_PATH := "res://addons/unityroom_sdk/client.gd"
-
-
 func _enable_plugin() -> void:
-	add_autoload_singleton(AUTOLOAD_NAME, AUTOLOAD_PATH)
-
+	pass
 
 func _disable_plugin() -> void:
-	remove_autoload_singleton(AUTOLOAD_NAME)
-
+	pass
 
 func _enter_tree() -> void:
 	pass
-
 
 func _exit_tree() -> void:
 	pass
